67bb6740cf60747be0ba4d068e37c1f39fc997c0
xiusiyan <xiusiyan@gmail.com>
2012-06-07 06:06:51 +0800

    test

---
+++ src/test/java/com/xiusiyan/myview/DALTest.java
@@ -0,0 +1,32 @@
+package com.xiusiyan.myview;
+
+import junit.framework.TestCase;
+
+/**
+ * DALTest
+ *
+ * @author xiusiyan
+ * @version 1.0, 2012-6-7
+ * @see
+ */
+public class DALTest extends TestCase {
+
+    /* (non-Javadoc)
+     * @see junit.framework.TestCase#setUp()
+     */
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    /* (non-Javadoc)
+     * @see junit.framework.TestCase#tearDown()
+     */
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+    
+    public void testInsertData(){
+        DAL.getInstance().insertData(1, 1, "a", "b");
+    }
+
+}
