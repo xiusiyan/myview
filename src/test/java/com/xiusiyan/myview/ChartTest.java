d2339d2a135e815381111082a453d3609f36dc61
xiusiyan <xiusiyan@gmail.com>
2012-07-27 02:45:45 +0800

    add

---
+++ src/test/java/com/xiusiyan/myview/ChartTest.java
@@ -0,0 +1,31 @@
+package com.xiusiyan.myview;
+
+import junit.framework.TestCase;
+
+/**
+ * ChartTest
+ *
+ * @author xiusiyan
+ * @version 1.0, 2012-7-27
+ * @see
+ */
+public class ChartTest extends TestCase {
+
+    /* (non-Javadoc)
+     * @see junit.framework.TestCase#setUp()
+     */
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    /* (non-Javadoc)
+     * @see junit.framework.TestCase#tearDown()
+     */
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void test01(){
+        
+    }
+}
